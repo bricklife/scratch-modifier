7bc31d86e8e214d425fdaa7df7b23b009d89c4c3
Shinichiro Oba <bricklife@gmail.com>
2023-11-08 21:22:47 +0900

    Enable to change the home URL

--- index.js
+++ index.js
@@ -12,6 +12,15 @@ try {
         const dest = path.join(scratchGuiDir, 'src/components/menu-bar/scratch-logo.svg');
         fs.copyFileSync(logoFile, dest);
     }
+
+    const homeUrl = core.getInput('home-url');
+    if (homeUrl) {
+        console.log(`home-url: '${homeUrl}'`);
+        const filePath = path.join(scratchGuiDir, 'src/playground/render-gui.jsx');
+        let code = fs.readFileSync(filePath, 'utf-8');
+        code = code.replace(/https:\/\/scratch.mit.edu/, 'http://bricklife.com');
+        fs.writeFileSync(filePath, code);
+    }
 } catch (error) {
     core.setFailed(error.message);
 }
