bc77a0231094725b5cc263e0d6f88e4a98087c19
Shinichiro Oba <bricklife@gmail.com>
2023-11-08 15:13:26 +0900

    Add a GitHub Action

---
+++ index.js
@@ -0,0 +1,17 @@
+const core = require('@actions/core');
+const path = require('path');
+const fs = require('fs');
+
+try {
+  const scratchGuiDir = core.getInput('scratch-gui-dir', { required: true});
+  console.log(`scratch-gui-dir: '${scratchGuiDir}'`);
+
+  const logoFile = core.getInput('logo-file');
+  if (logoFile) {
+    console.log(`logo-file: '${logoFile}'`);
+    const dest = path.join(scratchGuiDir, 'src/components/menu-bar/scratch-logo.svg');
+    fs.copyFileSync(logoFile, dest);
+  }
+} catch (error) {
+  core.setFailed(error.message);
+}
